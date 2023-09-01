355d4d5ed2c7556b7c53067556251a78bd4e54b1
Shivam-wz <56469003+Shivam-wz@users.noreply.github.com>
2023-09-01 16:58:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const axiosInstance = axios.create({
-  baseURL : 'https://crudcrud.com/api/49ab1f123b2e4d8888959f02d0cfc4c4/appointment'
+    baseURL : "https://crudcrud.com/api/71419404975640168311e8925ae8def4/generalstore"
 })
 let ul = document.querySelector('.display ul')
 
@@ -10,45 +10,15 @@ document.querySelector('.addItem form').addEventListener('submit', handleSubmit)
 ul.addEventListener('click', handleClick)
 
 
-
-
-async function handleSubmit(e){
-    try{
-    e.preventDefault();
-
-
-
-    const itemName = e.target["item-name"].value.trim()
-    const itemDesc = e.target["item-description"].value.trim()
-    const itemPrice = e.target["item-price"].value
-    const itemQty = e.target["item-qty"].value
-
-
-
-    if(itemName.trim().length === 0 || itemDesc.trim().length === 0 || itemPrice === "0" || itemQty === "0")
-        alert("Wrong input")
-
-    
-
-    const data ={
-        "item-name" : itemName,
-        "item-description" : itemDesc,
-        "item-price" : itemPrice,
-        "item-qty" : itemQty
-    }
-
-    let res;
-    let id ;
-
-    res = await axiosInstance.post('/', data)
-    console.log(res)
-    id = res.data._id
-
+function createLi(data,id){
     let li = document.createElement('li')
     const text = document.createElement("div")
 
-    text.textContent = `Item name : ${data["item-name"]} Description : ${data["item-description"]} Price : ${data["item-price"]} Quantity : ${data["item-qty"]}`
+    text.textContent = `Item name : ${data["item-name"]} Description : ${data["item-description"]} Price : ${data["item-price"]} Quantity : `
+    let span = document.createElement('span')
+    span.textContent = `${data["item-qty"]}`
     li.appendChild(text)
+    li.appendChild(span)
 
     const buttons = document.createElement("div")
 
@@ -72,12 +42,54 @@ async function handleSubmit(e){
     buttons.appendChild(buy3)
     li.appendChild(buttons)
 
+    return li
+}
+
+async function handleSubmit(e){
+    try{
+    e.preventDefault();
+
+
+
+    const itemName = e.target["item-name"].value.trim()  //         shivam
+    console.log(typeof itemName)
+    const itemDesc = e.target["item-description"].value.trim()
+    const itemPrice = e.target["item-price"].value
+    console.log(typeof itemPrice)
+    const itemQty = e.target["item-qty"].value
+
+
+
+    if(itemName.trim().length === 0 || itemDesc.trim().length === 0 || Number(itemPrice) <= 0 || Number(itemQty) <= 0){
+        alert("Wrong input")
+
+    }
+
+    
+else{
+    const data ={
+        "item-name" : itemName,
+        "item-description" : itemDesc,
+        "item-price" : itemPrice,
+        "item-qty" : itemQty
+    }
+
+    let res;
+    let id ;
+
+    res = await axiosInstance.post('/', data)
+    console.log(res)
+    id = res.data._id
+
+    let li = createLi(data,id)
+
     ul.appendChild(li)
 
     e.target["item-name"].value = ''
     e.target["item-description"].value = ''
     e.target["item-price"].value = ''
     e.target["item-qty"].value = ''
+}
 }catch(e){
     console.log(e)
 }
@@ -91,33 +103,7 @@ async function renderElements(e){
     res.data.forEach(data => {
         let id = data._id
 
-        let li = document.createElement('li')
-        const text = document.createElement("div")
-  
-        text.textContent = `Item name : ${data["item-name"]} Description : ${data["item-description"]} Price : ${data["item-price"]} Quantity : ${data["item-qty"]}`
-        li.appendChild(text)
-    
-        const buttons = document.createElement("div")
-
-        const buy1 = document.createElement('button')
-        buy1.classList.add('buy1')
-        buy1.textContent = "Buy1"
-        buy1.id = id
-    
-        const buy2 = document.createElement('button')
-        buy2.classList.add('buy2')
-        buy2.textContent = "Buy2"
-        buy2.id = id
-    
-        const buy3 = document.createElement('button')
-        buy3.classList.add('buy3')
-        buy3.textContent = "Buy3"
-        buy3.id = id
-    
-        buttons.appendChild(buy1)
-        buttons.appendChild(buy2)
-        buttons.appendChild(buy3)
-        li.appendChild(buttons)
+        let li = createLi(data,id)
     
         ul.appendChild(li)
     
@@ -134,6 +120,7 @@ async function handleClick(e){
 
         
         let id = e.target.id
+
         let str = e.target.parentNode.parentNode.firstElementChild.textContent;
         const itemName = str.substring(12,str.indexOf("Description")-1);
         const desc = str.substring(str.indexOf("Description") + 14  
@@ -141,7 +128,7 @@ async function handleClick(e){
         const price = Number(str.substring(str.indexOf("Price") + 8 
         , str.indexOf("Quantity ")-1))
 
-        const itemQty = Number(str.substring(str.lastIndexOf("Quantity") +11));
+        const itemQty = Number(e.target.parentNode.parentNode.firstElementChild.nextElementSibling.textContent);
 
 
         if(itemQty < buyQty)
@@ -157,8 +144,9 @@ async function handleClick(e){
 
         let res = await axiosInstance.put(`/${id}`,data)
         if(res.status === 200){
-            e.target.parentNode.parentNode.firstElementChild.textContent = 
-            `Item name : ${data["item-name"]} Description : ${data["item-description"]} Price : ${data["item-price"]} Quantity : ${data["item-qty"]}`
+
+            e.target.parentNode.parentNode.firstElementChild.nextElementSibling.textContent = 
+            `${data["item-qty"]}`
 
         }
         if(data["item-qty"] === 0 ){
